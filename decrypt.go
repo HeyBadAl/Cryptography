0f9858b31540b924bd12e5217531e1b4bc47b5f2
Badal <badalWorks1@gmail.com>
2023-09-25 10:08:11 +0530

    decrypt function for decryption ciphertext

---
+++ decrypt.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+	"log"
+)
+
+func decrypt(cipherText, key, iv string) string {
+	blockCipher, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	stream := cipher.NewCTR(blockCipher, []byte(iv))
+	bytes, err := hex.DecodeString(cipherText)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	stream.XORKeyStream(bytes, bytes)
+	return string(bytes)
+}
+
